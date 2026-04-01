6f5d3cc9040536b9f157daf3bd65a2e88e38e76b
muhammadkhalidhidayatullah29-code <muhammadkhalidhidayatullah29@gmail.com>
2026-04-01 14:30:25 +0700

    Prosedur untuk menerima inputan angka

--- praktikum.cpp
+++ praktikum.cpp
@@ -19,11 +19,16 @@ bool isFibonacci(int n) {
     if (n < 0) return false;
     int a = 0;
     int b = 1;
-    
+
     while (a < n) {
         int temp = a + b;
         a = b;
         b = temp;
     }
     return (a == n);
+}
+
+void inputAngka() {
+    cout << "Masukkan angka yang ingin dicek: ";
+    cin >> angka;
 }
